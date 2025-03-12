efb467929d227636697fabaa59db817cea469aad
chloekoee <chloe.koe@gmail.com>
2025-03-12 15:15:50 +1100

    Fix not being able to see within the 32 by 32 chunk as face culling was turned off

--- scene.py
+++ scene.py
@@ -1,6 +1,6 @@
 from settings import *
+import moderngl as mgl
 from world_objects.chunk import Chunk
-
 
 class Scene:
     def __init__(self, app):
@@ -13,45 +13,6 @@ class Scene:
     def render(self):
         self.chunk.render()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        ## By disabling face culling, we can see within the chunk (all voxels which are rendered are the ones which face outward to the camera)
+        ## this doesn't change - but instead of just having 3 faces rendered, all their faces are rendered ???
+        self.app.ctx.disable(mgl.CULL_FACE)
