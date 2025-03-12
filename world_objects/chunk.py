c49653f3d5e846b466d15c9933eddc6a5c08173f
chloekoee <chloe.koe@gmail.com>
2025-03-12 15:16:19 +1100

    Add rainbow colours by setting voxel value in chunk to be a function of the x,y,z

--- world_objects/chunk.py
+++ world_objects/chunk.py
@@ -25,8 +25,5 @@ class Chunk:
         for x in range(CHUNK_SIZE):
             for z in range(CHUNK_SIZE):
                 for y in range(CHUNK_SIZE):
-                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = 1
-                    # (
-                    #     x + y + z if int(glm.simplex(glm.vec3(x, y, z) * 0.1) + 1) else 0
-                    # )
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = x + y + z
         return voxels
