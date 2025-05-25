1d85133165896b9ac5f6f7c471ffe44317f0e620
chloekoee <110220808+chloekoee@users.noreply.github.com>
2025-05-25 16:55:54 +1000

    Update test tensor to generate seed

--- create_test_tensor.py
+++ create_test_tensor.py
@@ -2,15 +2,12 @@ import numpy as np
 
 # dimensions: (time, x, y, z, rgba)
 SIZE = 16
-tensor = np.zeros((2, SIZE, SIZE, SIZE, 4), dtype=np.float32)
+tensor = np.zeros((SIZE, SIZE, SIZE, 4), dtype=np.float32)
 
 # set full‐opacity alpha
-tensor[..., 3] = 0.7
+tensor[..., 3] = 1.0
 
 # frame 0: pink (1,0,1)
 tensor[0, ..., :3] = [1.0, 0.0, 0.7]
-
-# frame 1: orange (1,0.5,0)
-tensor[1, ..., :3] = [1.0, 0.5, 0.0]
 
 np.save("tensors/pink_orange_voxel.npy", tensor)
